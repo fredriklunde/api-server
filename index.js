bb79340f64762ad35bf8d627319777b63d6015db
Fredrik Lunde <fredrik.lunde3@gmail.com>
2019-01-09 16:42:40 +0100

    add port

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
+const port = process.env.PORT || 3090;
