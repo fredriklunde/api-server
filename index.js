3b831ade143c4eaf6fb5026c2b21ff792a1b245d
Fredrik Lunde <fredrik.lunde3@gmail.com>
2019-01-09 16:43:44 +0100

    add server

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const app = express();
+
 
 const port = process.env.PORT || 3090;
+const server = http.createServer(app);
