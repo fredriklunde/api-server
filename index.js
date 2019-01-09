717560395f17bb54b1639c50ee18d6bb1cecdaaa
Fredrik Lunde <fredrik.lunde3@gmail.com>
2019-01-09 16:52:06 +0100

    nodemon and other

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
 
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
