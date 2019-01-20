50e9ad88bf097f8b2a506ae2d4ecb89c99f87e86
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-20 09:14:14 +0100

    auth on route

--- services/passport.js
+++ services/passport.js
@@ -1,4 +1,4 @@
-const passport = requirt('passport');
+const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
