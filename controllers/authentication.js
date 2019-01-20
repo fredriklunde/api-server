1874b8f30aefa9511fe0a916a1875c0d985760b5
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-20 08:55:37 +0100

    User Token

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,6 +1,11 @@
 const User = require('../models/user');
 const jwt = require('jwt-simple');
-const comfig = require('../config'); 
+const config = require('../config'); 
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
     const email = req.body.email;
@@ -25,7 +30,7 @@ exports.signup = function(req, res, next) {
         user.save(function(err) {
             if(err) { return next(err); }
 
-            res.json({ success: true });
+            res.json({ token: tokenForUser(user) });
         });
     });
 }
